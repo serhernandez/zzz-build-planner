019fc548420442f0a863084a201b010fef472f35
serhernandez <sahernandez1512@gmail.com>
2026-01-05 12:54:23 -0600

    Add test agents and materials

---
+++ src/data/agents.js
@@ -0,0 +1,74 @@
+export const agents = {
+    alice: {
+        id: "alice",
+        name: "Alice Thymefield",
+        icon: "public\\assets\\agents\\icons\\Agent_Alice_Thymefield_Icon.png",
+        rank: "S",
+        attribute: "Physical",
+        specialty: "Anomaly",
+        faction: "Spook Shack"
+    },
+    anby: {
+        id: "anby",
+        name: "Anby Demara",
+        icon: "public\\assets\\agents\\icons\\Agent_Anby_Demara_Icon.png",
+        rank: "A",
+        attribute: "Electric",
+        specialty: "Stun",
+        faction: "Cunning Hares"
+    },
+    billy: {
+        id: "billy",
+        name: "Billy Kid",
+        icon: "public\\assets\\agents\\icons\\Agent_Billy_Kid_Icon.png",
+        rank: "A",
+        attribute: "Physical",
+        specialty: "Attack",
+        faction: "Cunning Hares"
+    },
+    dialyn: {
+        id: "dialyn",
+        name: "Dialyn",
+        icon: "public\\assets\\agents\\icons\\Agent_Dialyn_Icon.png",
+        rank: "S",
+        attribute: "Physical",
+        specialty: "Stun",
+        faction: "Krampus Compliance Authority"
+    },
+    miyabi: {
+        id: "miyabi",
+        name: "Hoshimi Miyabi",
+        icon: "public\\assets\\agents\\icons\\Agent_Hoshimi_Miyabi_Icon.png",
+        rank: "S",
+        attribute: "Frost",
+        specialty: "Anomaly",
+        faction: "Hollow Special Operations Section 6"
+    },
+    nicole: {
+        id: "nicole",
+        name: "Nicole Demara",
+        icon: "public\\assets\\agents\\icons\\Agent_Nicole_Demara_Icon.png",
+        rank: "A",
+        attribute: "Ether",
+        specialty: "Support",
+        faction: "Cunning Hares"
+    },
+    pan: {
+        id: "pan",
+        name: "Pan Yinhu",
+        icon: "public\\assets\\agents\\icons\\Agent_Pan_Yinhu_Icon.png",
+        rank: "A",
+        attribute: "Fire",
+        specialty: "Defense",
+        faction: "Yunkui Summit"
+    },
+    yidhari: {
+        id: "yidhari",
+        name: "Yidhari Murphy",
+        icon: "public\\assets\\agents\\icons\\Agent_Yidhari_Murphy_Icon.png",
+        rank: "S",
+        attribute: "Ice",
+        specialty: "Rupture",
+        faction: "Spook Shack"
+    },
+};
---
+++ src/data/materials.js
@@ -0,0 +1,10 @@
+export const materials = {
+    senior_investigation_log: {
+        id: "senior_investigation_log",
+        name: "Senior Investigation Log",
+        icon: "public\\assets\\agents\\Item_Senior_Investigator_Log.png",
+        rank: "A",
+        expValue: 3000,
+        type: "agent_exp"
+    }
+};
